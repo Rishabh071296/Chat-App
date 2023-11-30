0209db5c2949c947f3ca4a0dff99a927da65ea3b
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-11-30 12:24:21 +0530

    Update

--- chat-app/src/route.js
+++ chat-app/src/route.js
@@ -1,9 +1,10 @@
-import PublicChat from "./components/PublicChat";
-import RoomChat from "./components/RoomChat";
-import LiveVisitors from "./components/Live Visitors";
+import PublicChat from "./components/PublicChat"
+import RoomChat from "./components/RoomChat"
+import LiveVisitors from "./components/Live Visitors"
+
 
 export default [
-    { path: "/", exact: true, Component: PublicChat },
-    { path: "/roomChat", Component: RoomChat },
-    { path: "/liveVisitors", Component: LiveVisitors }
-  ];
+  { path: "/publicChat", exact: true, Component: PublicChat },
+  { path: "/roomChat", Component: RoomChat },
+  { path: "/liveVisitors", Component: LiveVisitors }
+];
